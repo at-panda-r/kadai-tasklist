d96fef1abcee09fc8bc656c0f4bba6cf00b777a8
at-panda-t <noreply@github.com>
2020-06-22 05:10:09 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -48,10 +48,11 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'title' => 'required|max:10',   // 追加
             'content' => 'required|max:191',
         ]);
-
         $task = new Task;
+        $task->title = $request->title;
         $task->content = $request->content;
         $task->save();
 
@@ -101,10 +102,11 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'title' => 'required|max:10',   // 追加
             'content' => 'required|max:191',
         ]);
-
         $task = Task::find($id);
+        $task->title = $request->title;
         $task->content = $request->content;
         $task->save();
 
